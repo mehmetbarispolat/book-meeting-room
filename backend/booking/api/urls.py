a6ffd58463a8cd7d2118f090e42d36ea0514af4d
mehmetbarispolat <mehmetbarispolat@gmail.com>
2024-04-12 19:39:37 +0300

    [BMR] Added addroom and listroom endpoints

--- backend/booking/api/urls.py
+++ backend/booking/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AvailableRooms, BookRoom
+from .views import AvailableRooms, BookRoom, AddRoomView, ListRoomsView
 
 urlpatterns = [
     path('availablerooms', AvailableRooms.as_view(), name='available-rooms'),
     path('bookroom', BookRoom.as_view(), name='book-room'),
+    path('api/addroom', AddRoomView.as_view(), name='add_room'),
+    path('api/listrooms', ListRoomsView.as_view(), name='list_rooms'),
 ]
--- backend/booking/api/views.py
+++ backend/booking/api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 from .models import Room, Booking
 from .serializers import RoomSerializer, BookingSerializer, UserSerializer
@@ -44,3 +44,21 @@ class BookRoom(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+class AddRoomView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        serializer = RoomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class ListRoomsView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
