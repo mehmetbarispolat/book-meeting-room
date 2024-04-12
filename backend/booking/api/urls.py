a432454a70d5630cdc220f277b11bc5783bd7945
mehmetbarispolat <mehmetbarispolat@gmail.com>
2024-04-12 20:46:03 +0300

    [BMR] Adjusted frontend structure

--- backend/booking/api/urls.py
+++ backend/booking/api/urls.py
@@ -4,6 +4,6 @@ from .views import AvailableRooms, BookRoom, AddRoomView, ListRoomsView
 urlpatterns = [
     path('availablerooms', AvailableRooms.as_view(), name='available-rooms'),
     path('bookroom', BookRoom.as_view(), name='book-room'),
-    path('api/addroom', AddRoomView.as_view(), name='add_room'),
-    path('api/listrooms', ListRoomsView.as_view(), name='list_rooms'),
+    path('addroom', AddRoomView.as_view(), name='add_room'),
+    path('listrooms', ListRoomsView.as_view(), name='list_rooms'),
 ]
